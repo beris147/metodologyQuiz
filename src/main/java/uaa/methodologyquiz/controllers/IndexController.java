f1af793e8aace8290933bac47d041ad55f3e88c2
root <edwir147@gmail.com>
2020-10-06 13:20:02 -0500

    first commit

---
+++ src/main/java/uaa/methodologyquiz/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package uaa.methodologyquiz.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author root
+ */
+public class IndexController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
